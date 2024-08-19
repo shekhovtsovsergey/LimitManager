f2c5b078aa572c94f7a3e06e1130b839a80f322b
shekhovtsovsergey <89629417575@mail.ru>
2024-08-19 16:59:48 +0300

    Lesson17

--- src/main/java/ru/shekhovtsov/controller/LimitController.java
+++ src/main/java/ru/shekhovtsov/controller/LimitController.java
@@ -2,7 +2,6 @@ package ru.shekhovtsov.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.shekhovtsov.dto.LimitDto;
-import ru.shekhovtsov.model.Limit;
 import ru.shekhovtsov.service.LimitService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
